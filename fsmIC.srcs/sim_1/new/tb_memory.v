2715a663aa251d03dbaefefe1f4274a0bcb89c26
jens636 <jens_636@hotmail.com>
2024-10-24 17:45:25 +0200

    tt

--- fsmIC.srcs/sim_1/new/tb_memory.v
+++ fsmIC.srcs/sim_1/new/tb_memory.v
@@ -2,20 +2,24 @@
 
 module tb_memory;
     
+    reg t_clk;
     reg [2:0] t_k;
     reg [7:0] t_indata;
     reg t_op;
     wire [7:0] t_outdata;
+    wire t_valid;
     
     memory M2 (
+        .clk (t_clk),
         .k_address (t_k),
         .n_data_in (t_indata),
         .op (t_op),
-        .n_data_out (t_outdata)
+        .n_data_out (t_outdata),
+        .valid (t_valid)
         );
         
    always #30 t_op = ~t_op;     
-   
+   always #10 t_clk = ~t_clk;
    initial begin
     #800 $finish;
    end
@@ -24,6 +28,7 @@ module tb_memory;
         t_k = 3'b000;
         t_indata = 8'b00000000;
         t_op = 1'b0;
+        t_clk = 1'b0;
    end
    
    initial begin
