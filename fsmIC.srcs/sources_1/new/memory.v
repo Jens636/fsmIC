1fa2cc0718f9e69821d4897991d213eb8667a928
jens636 <jens_636@hotmail.com>
2024-10-24 17:39:31 +0200

    update

--- fsmIC.srcs/sources_1/new/memory.v
+++ fsmIC.srcs/sources_1/new/memory.v
@@ -1,14 +1,17 @@
 `timescale 1ns / 1ps
-module memory(k_address, n_data_in, op, n_data_out);
+module memory(clk, k_address, n_data_in, op, n_data_out, valid);
 
     input [2:0] k_address;
     input [7:0] n_data_in;
     input op;
+    input clk;
     output [7:0] n_data_out;
+    output valid;
     
     wire [7:0] select;
-    
+    wire rw;
     decoder3to8 inst1 (k_address, select);
-    memory8bit inst2[7:0] (select, n_data_in, op, n_data_out);
+    fsmIC inst2[7:0] (clk, op, select, rw, valid);
+    memory8bit inst3[7:0] (select, n_data_in, rw, n_data_out);
     
 endmodule
