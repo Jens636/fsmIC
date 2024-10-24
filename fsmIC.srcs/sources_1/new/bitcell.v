36be574187297144ea2be7dda4d58b8e4398d59d
jens636 <jens_636@hotmail.com>
2024-10-24 17:08:48 +0200

    bitcell done2*

--- fsmIC.srcs/sources_1/new/bitcell.v
+++ fsmIC.srcs/sources_1/new/bitcell.v
@@ -8,10 +8,10 @@ module bitcell(sel, inp, rw, outp);
     wire S, R;
     wire Q, Q_neg;
     
-    nand(S, sel, inp, ~rw);
-    nand(R, sel, ~inp, ~rw);
+    nand(S, sel, inp, rw);
+    nand(R, sel, ~inp, rw);
     
     SR_latch inst1 (S, R, Q, Q_neg);
-    and (outp, sel, Q, rw);
+    and(outp, sel, Q, ~rw);
   
 endmodule
